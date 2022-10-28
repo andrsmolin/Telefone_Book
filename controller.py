0c28bfb8475dcf8d0bab662151490a5b519f7793
andrsmolin <acord.81@mail.ru>
2022-10-28 20:57:35 +0300

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,16 @@
+import modul_use_telefone_book as mt
+from modul_use_telefone_book import  read, delete #add,
+import preview as pr
+
+
+def choise_type(type):
+    if type == 1:
+        return mt.read()
+    elif type == 2:
+        return mt.create()
+    elif type == 3:
+        return mt.delete()
+    # elif type == 4:
+    #     return mt.add()
+    elif type == 5:
+        return pr.create()
