362d624d6aa434b167b5a26cb4bed9b3c7b07f57
andrsmolin <acord.81@mail.ru>
2022-10-28 20:57:42 +0300

    Create modul_use_telefone_book.py

---
+++ modul_use_telefone_book.py
@@ -0,0 +1,55 @@
+from os import statvfs_result
+from unicodedata import name
+import view as v
+
+
+def read(): 
+    tel_dect = return_data()
+    surname = v.input_surname()
+    count = 0
+    for i in range(1, len(tel_dect)+1):
+        if tel_dect[i]['surname'].find(surname) > 0:
+            print(tel_dect[i])#['surname']['name']['telefon']['status'])
+            count += 1
+    else:
+        if count == 0 :
+            print("Контакт не найден")
+
+def delete():
+    idd = int(v.input_id())
+    tel_dect = return_data()
+    tel_dect[idd] = {'idd' : idd, 'surname' : 'delete', 'name' : 'delete', 'telefon' : '_', 'status' : '_'}
+    exp_dect(tel_dect)
+
+def create():
+    tel_dect = return_data()
+    surname = v.input_surname()
+    name = v.input_name()
+    number_person = v.input_number()
+    status = v.input_status()
+    i = len(tel_dect)+1
+    tel_dect[i] = {'idd': i,'surname': surname, 'name': name, 'telefon': number_person, 'status': status}
+    exp_dect(dict(tel_dect))
+    print(tel_dect[i])
+
+def return_data():
+    imp_dect = {}
+    with open("E:/IT_HomeWork/tel.txt", 'r', encoding='utf-8') as f:
+        for line in f :
+            key, sur, nam, tel, stat_ = line.split()
+            imp_dect[int(key)] = {'idd': key, 'surname' : sur, 'name' : nam, 'telefon' : tel, 'status' : stat_}
+    return imp_dect
+
+def exp_dect(tel_dect):
+    f = open('E:/IT_HomeWork/tel.txt', 'w', encoding ='utf-8')
+    for i in range(1, len(tel_dect)+1):
+        f.write(str(tel_dect[i]['idd']) + '  ')
+        f.write(tel_dect[i]['surname'] + ' ')
+        f.write(tel_dect[i]['name']+ ' ')
+        f.write(tel_dect[i]['telefon'] + ' ')
+        f.write(tel_dect[i]['status'] + '\n')
+        # print(tel_dect[i]['name'], tel_dect[i]['surname'], tel_dect[i]['telefon'])
+    f.close()
+
+tel_dect = {}
+
